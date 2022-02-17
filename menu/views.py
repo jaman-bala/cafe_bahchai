f7f8b1bbb1d92352a9829e847b1fc5a3c61745d4
jaman-bala <ermekoffdastan@yandex.ru>
2022-02-18 01:57:38 +0600

    update info

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import FormMixin
 # Create your views here.
+
+from .models import Category, Product
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+    context_object_name = "products"
+
+
+class AboutView(TemplateView):
+    template_name = "about-1.html"
+
+
+class CareersView(TemplateView):
+    template_name = "careers.html"
+
+
+class Error404View(TemplateView):
+    template_name = "404.html"
+
+
+class ListsView(TemplateView):
+    template_name = "components/lists.html"
+
+
+class MapsView(TemplateView):
+    template_name = "components/maps.html"
+
+
+class ProductsView(ListView):
+    model = Product
+    template_name = "menu-grid.html"
+    context_object_name = 'products_grid'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
